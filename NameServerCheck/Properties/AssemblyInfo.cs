993277bf75d72fdb7ba5134af1e27bfb9704891c
Michael Fight <michaelkaempf1@gmail.com>
2019-11-07 12:55:28 +0100

    Version 1.0.1.0

--- NameServerCheck/Properties/AssemblyInfo.cs
+++ NameServerCheck/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("Name Server Check")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Server information lookup programm")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("smile solutions gmbh")]
 [assembly: AssemblyProduct("NameServerCheck")]
@@ -51,5 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
